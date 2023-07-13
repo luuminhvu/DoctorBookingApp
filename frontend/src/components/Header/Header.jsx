bdf655400e1d10dd64b03608345939356f1577d2
Luu Vu <vu7178@gmail.com>
2023-07-14 00:25:39 +0700

    header #1

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,69 @@
+// import { useEffect, useRef } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import userImg from '../../assets/images/avatar-icon.png';
+const navLinks = [
+    {
+        path: '/home',
+        display: 'Home',
+    },
+    {
+        path: '/doctors',
+        display: 'Find a Doctors',
+    },
+    {
+        path: '/services',
+        display: 'Services',
+    },
+    {
+        path: '/contact',
+        display: 'Contact',
+    },
+];
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className="header flex items-center">
+            <div className="container">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="navigation">
+                        <ul className="menu flex items-center gap-[2.7rem]">
+                            {navLinks.map((link, index) => (
+                                <li key={index}>
+                                    <NavLink
+                                        to={link.path}
+                                        className={(navClass) =>
+                                            navClass.isActive
+                                                ? 'text-primaryColor text-[16px] leading-7 font-[600]'
+                                                : 'text-textColor text-[16px] leading-7 font-[500] hover:text-textPrimaryColor'
+                                        }
+                                    >
+                                        {link.display}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div>
+                            <Link to="/">
+                                <figure className="w-35px h-35px rounded-full cursor-pointer">
+                                    <img src={userImg} className="w-full rounded-full" alt="" />
+                                </figure>
+                            </Link>
+                        </div>
+                        <Link to="/login">
+                            <button className="h-[44px] bg-primaryColor py-2 px-6 text-white font-[600] flex items-center justify-center rounded-[50px]">
+                                Login
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
