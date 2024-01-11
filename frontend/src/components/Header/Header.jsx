8effc76aad2e3188a7bec4b8adc39aa43b859db5
Luu Vu <vu7178@gmail.com>
2024-01-11 19:05:21 +0700

    home_part1

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,8 @@
 import { NavLink, Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import userImg from '../../assets/images/avatar-icon.png';
+import { BiMenu } from 'react-icons/bi';
+import { useEffect, useRef } from 'react';
 const navLinks = [
     {
         path: '/home',
@@ -21,14 +23,34 @@ const navLinks = [
     },
 ];
 const Header = () => {
+    const headerRef = useRef(null);
+    const menuRef = useRef(null);
+    const handleStickyHeader = () => {
+        window.addEventListener('scroll', () => {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                headerRef.current.classList.add('sticky_header');
+            } else {
+                headerRef.current.classList.remove('sticky_header');
+            }
+        });
+    };
+    const toggleMenu = () => {
+        menuRef.current.classList.toggle('show_menu');
+    };
+    useEffect(() => {
+        handleStickyHeader();
+        return () => {
+            window.removeEventListener('scroll', handleStickyHeader);
+        };
+    }, []);
     return (
-        <header className="header flex items-center">
+        <header className="header flex items-center" ref={headerRef}>
             <div className="container">
                 <div className="flex items-center justify-between">
                     <div>
                         <img src={logo} alt="" />
                     </div>
-                    <div className="navigation">
+                    <div className="navigation" ref={menuRef} onClick={toggleMenu}>
                         <ul className="menu flex items-center gap-[2.7rem]">
                             {navLinks.map((link, index) => (
                                 <li key={index}>
@@ -54,11 +76,17 @@ const Header = () => {
                                 </figure>
                             </Link>
                         </div>
-                        <Link to="/login">
-                            <button className="h-[44px] bg-primaryColor py-2 px-6 text-white font-[600] flex items-center justify-center rounded-[50px]">
-                                Login
-                            </button>
-                        </Link>
+
+                        <div>
+                            <Link to="/login">
+                                <button className="h-[44px] bg-primaryColor py-2 px-6 text-white font-[600] flex items-center justify-center rounded-[50px]">
+                                    Login
+                                </button>
+                            </Link>
+                        </div>
+                        <span className="md:hidden" onClick={toggleMenu}>
+                            <BiMenu className="w-6 h-6 cursor-pointer" />
+                        </span>
                     </div>
                 </div>
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,49 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <>
+            <section className="hero__section pt-[60px] 2xl:h-[800px]">
+                <div className="container">
+                    <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+                        <div>
+                            <div className="lg:w-[570px]">
+                                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                                    We help patients live a healthy, longer life
+                                </h1>
+                                <p className="text__para">
+                                    We provide a wide range of medical services for patients from all over the world. We
+                                    help patients to live a healthy and happy life.
+                                </p>
+                                <button className="btn">Request an Appointment</button>
+                            </div>
+                            <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                                <div>
+                                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                                        30+
+                                    </h2>
+                                    <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                                    <p className="text__para">Years of Experience</p>
+                                </div>
+                                <div>
+                                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                                        15+
+                                    </h2>
+                                    <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                                    <p className="text__para">Clinic Location</p>
+                                </div>
+                                <div>
+                                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                                        100%
+                                    </h2>
+                                    <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                                    <p className="text__para">Patient Satisfaction</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Home;
